84ca50d4380c54b334e52eda354557e80882042e
aswanijehangeer <jahangiraswani237@gmail.com>
2023-10-24 22:56:24 +0500

    added: line, bar and heatmap functionality

--- server.R
+++ server.R
@@ -1,7 +1,5 @@
-# Define server logic ----
+# Server logic ----
 server <- function(input, output, session) {
-  
-  
   # Filtering data based on user inputs ----
   filtered_data <- reactive({
 
@@ -33,6 +31,20 @@ server <- function(input, output, session) {
 
     return(filter_data)
   })
+  
+  # Shiny-alert if there is no data for selected inputs ----
+  observe({
+    if (nrow(filtered_data()) == 0) {
+      shinyalert(
+        title = "Data Not Available", 
+        text = "Data is not available for the selected inputs.",
+        type = "error",
+        animation = TRUE,
+        confirmButtonCol = "#007BC2",
+        confirmButtonText = "BACK"
+      )
+    }
+  })
 
   # total number of customers
   num_customers <- reactive({
@@ -56,7 +68,7 @@ server <- function(input, output, session) {
     sum(filtered_data$Profit)
   })
 
-  # Rendering the value boxes
+  # Rendering the value boxes ----
   output$total_customers <- renderText({
     num_customers()
   })
@@ -68,4 +80,63 @@ server <- function(input, output, session) {
   output$total_profit <- renderText({
     total_profit()
   })
+  
+  # Monthly Sales chart ----
+  output$monthly_sales <- renderEcharts4r({
+    
+    filtered_data <- filtered_data()
+    
+    month_sales <- filtered_data |> 
+      group_by(Month) |> 
+      summarize(Sales = sum(Sales))
+    
+    month_sales |> 
+      e_charts(Month) |> 
+      e_line(Sales) |> 
+      e_color("#007BC2") |> 
+      e_legend(show = FALSE) |> 
+      e_tooltip(trigger = "item") |> 
+      e_toolbox_feature(feature = c("saveAsImage", "dataView"))
+    
+  })
+  # Product Sales chart ----
+  output$product_sales <- renderEcharts4r({
+    
+    filtered_data <- filtered_data()
+    
+    product_sales <- filtered_data |>
+      group_by(`Product Name`) |>
+      summarize(Sales = sum(Sales)) |>
+      arrange(desc(Sales)) |>
+      top_n(15)
+    
+    product_sales |> 
+      e_charts(`Product Name`) |> 
+      e_bar(Sales) |> 
+      e_color("#007BC2") |> 
+      e_legend(show = FALSE) |> 
+      e_tooltip(trigger = "item") |> 
+      e_toolbox_feature(feature = c("saveAsImage", "dataView")) |> 
+      e_x_axis(axisLabel = 'none')
+  })
+  # Heat-map - The Number of Orders by Months and Years ----
+  output$heat_map <- renderEcharts4r({
+    
+    filtered_data <- filtered_data()
+    
+    order_count <- filtered_data |>
+      group_by(Year, Month) |>
+      summarise(Orders = n(), .groups = 'drop')
+    
+    order_count |>
+      e_charts(Month) |>
+      e_heatmap(Year, Orders) |>
+      e_visual_map(Orders,
+                   inRange = list(color = c("#007BC2", "#4CBDF5", "#005B95"))) |>
+      e_tooltip(trigger = "item") |>
+      e_legend(show = FALSE) |> 
+      e_toolbox_feature(feature = c("saveAsImage", "dataView")) |> 
+      e_x_axis(axisLabel = 'none')
+  })
+  
 }
